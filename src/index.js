31a64848a9132ce8fc048603d8f3d22f7b6c8668
Joseph Murray <josephwmurray@gmail.com>
2023-08-16 14:19:53 -0700

    set up api call function in apicall.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { APICall } from './js/apicall';
+
 
 window.addEventListener("load", function() {
 });
---
+++ src/js/apicall.js
@@ -0,0 +1,24 @@
+export class APICall {
+  static async getRates() {
+    const API_KEY = xxxx;
+    const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const currency = data.conversion_rates;
+      
+      if(currency) {
+        const euro = currency.EUR;
+        const russia = currency.RUS;
+        const canada = currency.CAD;
+        const egypt = currency.EGP;
+        const gold = currency.XAU;
+        return {euro, russia, canada, egypt, gold};
+      } else {
+        return null;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+}
