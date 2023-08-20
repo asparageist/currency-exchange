0858191d7d3be246373027d9be1a3cfbd9afce68
Joseph Murray <josephwmurray@gmail.com>
2023-08-19 21:43:16 -0700

    fix dotenv webpack issues

--- src/js/apicall.js
+++ src/js/apicall.js
@@ -2,7 +2,10 @@ export class APICall {
   static async getRates() {
     const API_KEY = process.env.API_KEY;
     const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
-      const response = await fetch(url);
+    console.log(url);
+      const response = await fetch(url, {
+        mode: 'no-cors'
+      });
       const data = await response.json();
       const currency = data.conversion_rates;
       
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const { ESLint } = require('eslint');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -15,6 +16,7 @@ module.exports = {
   },
   devtool: 'eval-source-map',
   plugins: [
+    new Dotenv(),
     new ESLintPlugin(),
     new CleanWebpackPlugin({
       verbose: true
